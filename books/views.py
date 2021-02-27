a8f740f5334a51709990e77d90686a536f376e25
majed <majed>
2021-02-28 01:10:48 +0400

    update

--- books/views.py
+++ books/views.py
@@ -15,7 +15,9 @@ def save_book_form(request, form, template_name):
     data = dict()
     if request.method == 'POST':
         if form.is_valid():
-            form.save()
+            obj=form.save()
+            print('_'*80)
+            print(obj)
             data['form_is_valid'] = True
             books = Book.objects.all()
             data['html_book_list'] = render_to_string('books/includes/partial_book_list.html', {
@@ -31,6 +33,7 @@ def save_book_form(request, form, template_name):
 def book_create(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
+        
     else:
         form = BookForm()
     return save_book_form(request, form, 'books/includes/partial_book_create.html')
@@ -39,7 +42,13 @@ def book_create(request):
 def book_update(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
+        print('a'*80)
+        print("i am in book update hi")
+        print('a'*80)
+        print(book)
         form = BookForm(request.POST, instance=book)
+        print('d'*80)
+        print(request.POST.get("mail"))
     else:
         form = BookForm(instance=book)
     return save_book_form(request, form, 'books/includes/partial_book_update.html')
---
+++ runserver.bat
@@ -0,0 +1,5 @@
+
+@echo off
+cd /d C:\Users\M.E\venv\scripts\activate.bat & cd /d C:\Users\M.E\python_project\WorkTest\cvresume 3 app use json\ & python manage.py runserver 2022"
+
+start C:\"Program Files (x86)"\Google\Chrome\Application\chrome.exe "http://127.0.0.1:8000/"
